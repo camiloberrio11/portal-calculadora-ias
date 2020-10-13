20d405bf379a104f62b6c4846a802aed4bece4b3
jberrio <jberrio@transfiriendo.com>
2020-10-13 07:48:02 -0500

    Auto-generated commit

--- assets/parameter/parameter.js
+++ assets/parameter/parameter.js
@@ -1,3 +1,3 @@
 var data = new Object();
 //data.basePath = "http://localhost:8094";
-data.basePath = "https://api-calculadora-ias.herokuapp.com/";
+data.basePath = "https://api-calculadora-ias.herokuapp.com";
